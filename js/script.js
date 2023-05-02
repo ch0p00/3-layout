66c8a9fde84aa2e4a99288c86bbea270695562a6
ch0p00 <131778128+ch0p00@users.noreply.github.com>
2023-05-02 17:46:21 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const burgerMenu = document.querySelector('.burger-menu');
+const menu = document.querySelector('.menu');
+
+burgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.toggle('open');
+  menu.classList.toggle('open');
+});
+
+// Hide the menu when a link is clicked
+menu.addEventListener('click', () => {
+  burgerMenu.classList.remove('open');
+  menu.classList.remove('open');
+});
