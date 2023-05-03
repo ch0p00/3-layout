af3fd5653754e523ffe4248aba8d3002f8295fec
ch0p00 <131778128+ch0p00@users.noreply.github.com>
2023-05-03 16:16:44 +0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
     $('.slider').slick({
         autoplay:true,
-        autoplaySpeed:4000
+        autoplaySpeed:4000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
 });
